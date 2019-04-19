289294e1cd7840508ed09c0b394944d5c421f58c
rbs517 <rebeccaskurnik@gmail.com>
2019-04-18 20:57:42 -0400

    adding files

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var request = require('request');
 var port = 8030;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.redirect('));
+app.get('/', (req, res) => res.redirect('public'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
